2e59503a0d798a3c49319c6b447f1927ff533d29
Philipp Resch <phil@2kd.de>
2018-06-28 10:44:36 +0200

    Reponame change

--- main.go
+++ main.go
@@ -8,11 +8,11 @@ import (
 	"sync"
 	"time"
 
-	"github.com/dapaxx/prometheus-amqp/amqp"
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
 	"github.com/gogo/protobuf/proto"
 	"github.com/golang/snappy"
+	"github.com/phires/prometheus-amqp/amqp"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/common/model"
 	"github.com/prometheus/common/promlog"
